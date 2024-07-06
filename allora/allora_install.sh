6ed2c13a68e177e7a348f75ab4a73ddf8d1c2b77
lionc <shizijian2010@yahoo.com.hk>
2024-07-06 10:27:24 +0800

    add uninstall airchain function

--- allora/allora_install.sh
+++ allora/allora_install.sh
@@ -21,6 +21,14 @@ function check_block_status(){
     curl -s http://localhost:26657/status | jq .
 }
 
+function uninstall_node(){
+    cd
+    rm -r allora-chain
+    echo "=========================="
+    echo "Uninstallation complete"
+    echo "=========================="
+}
+
 function main_menu() {
   while true; do
       clear
@@ -28,12 +36,14 @@ function main_menu() {
       echo "1. Install Node"
       echo "2. View Logs"
       echo "3. Block Sync Staus"
-      read -p "Please input (1-3): " OPTION
+      echo "4. Uninstall Node"
+      read -p "Please input (1-4): " OPTION
 
       case $OPTION in
           1) install_node ;;
           2) inspect_logs ;;
           3) check_block_status ;;
+          4) uninstall_node ;;
           *) echo "Invalid Choice." ;;
       esac
       echo "Press any key back to menu..."
