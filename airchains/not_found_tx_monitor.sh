8f414c97bc6ef7413f29a9ff56e6fbfabdd84901
lionc <shizijian2010@yahoo.com.hk>
2024-07-11 21:41:03 +0800

    update not_found_tx_monitor.sh

--- airchains/not_found_tx_monitor.sh
+++ airchains/not_found_tx_monitor.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 service_name="stationd"
-not_found_tx_string="Failed to get transaction by hash: not found"
+error_strings=("Failed to get transaction by hash: not found" "Failed to Transact Verify pod" "Switchyard client connection error")
 restart_delay=180  # Restart delay in seconds (3 minutes)
 
 echo "Script started and it will restart $service_name if needed..."
 
 while true; do
-    # Get the last 10 lines of service logs
-  logs=$(systemctl status "$service_name" --no-pager | tail -n 1)
+    # Get the last line of the service logs
+    logs=$(journalctl -u "$service_name" -n 1 --no-pager | tail -n 1)
 
-    # Check for both error and gas used strings
-  if [[ "$logs" =~ $not_found_tx_string ]]; then
-    echo "Found error and gas used in logs, stopping $service_name..."
-    systemctl restart "$service_name"
-    echo "Service $service_name started"
+    # Check for each error string in the logs
+    for error_string in "${error_strings[@]}"; do
+        if [[ "$logs" == *"$error_string"* ]]; then
+            echo "Found error string '$error_string' in logs, restarting $service_name..."
+            systemctl restart "$service_name"
+            echo "Service $service_name restarted"
+            break
+        fi
+    done
 
-  fi
-
-  # Sleep for the restart delay
-  sleep "$restart_delay"
-
+    # Sleep for the restart delay
+    sleep "$restart_delay"
 done
