fc4480f5ad52faa274009febb7c2540b10918400
lionc <shizijian2010@yahoo.com.hk>
2024-11-11 20:31:25 +0800

    add soneium sh

---
+++ soneium/soneium.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+
+function update_script(){
+  wget -O sonaric_helper.sh https://raw.githubusercontent.com/LIONSHI01/nodes_management/main/sonaric/sonaric_helper.sh && chmod +x sonaric_helper.sh && ./sonaric_helper.sh
+}
+
+
+
+
+function install_dependencies(){
+  sudo apt install ufw
+  sudo ufw allow 9545
+}
+
+
+function install(){
+  install_dependencies
+
+  VPS_IP=$(hostname -I | awk '{print $1}')
+  read -p "Enter Private Key : " PRIVATE_KEY
+
+  # Pull repo
+  git clone https://github.com/Soneium/soneium-node.git
+  cd soneium-node/minato
+
+  # Add Jwt file
+  echo "$PRIVATE_KEY" > jwt.txt
+
+
+  # Update .env values
+  cp sample.env .env
+  sed -i "s|L1_URL=.*|L1_URL=https://ethereum-sepolia-rpc.publicnode.com|" .env
+  sed -i "s|L1_BEACON=.*|L1_BEACON=https://ethereum-sepolia-beacon-api.publicnode.com|" .env 
+  sed -i "s|P2P_ADVERTISE_IP=.*|P2P_ADVERTISE_IP=$VPS_IP|" .env
+
+  # Replace IP in docker-compose.yml
+  sed -i "s|--nat=extip:<your_node_public_ip>|--nat=extip:$VPS_IP|" docker-compose.yml
+
+}
+
+function view_logs(){
+  cd soneium-node/minato
+  docker-compose logs -f op-node-minato
+}
+
+function restart_node(){
+  cd soneium-node/minato
+  docker-compose down
+  docker-compose up -d
+}
+
+function stop_node(){
+  cd soneium-node/minato
+  docker-compose down
+}
+
+
+
+function main_menu() {
+  while true; do
+      clear
+      echo "Please choose the command to execute:"
+      echo "1. Install"
+      echo "2. View Logs"
+      echo "3. Restart Node"
+      echo "4. Stop Node"
+      echo "0. Update Command"
+      read -p "Please input (0-4): " OPTION
+
+      case $OPTION in
+          1) install ;;
+          2) view_logs ;;
+          3) restart_node ;;
+          4) stop_node ;;
+          0) update_script ;;
+          *) echo "Invalid Choice." ;;
+      esac
+      echo "Press any key back to menu..."
+      read -n 1
+  done
+}
+
+main_menu
