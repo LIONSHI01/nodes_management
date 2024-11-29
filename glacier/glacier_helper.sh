ea29544c1df208cdead9169d7d67a3fb0ee55b92
lionc <shizijian2010@yahoo.com.hk>
2024-11-29 21:10:48 +0800

    update glacier

--- glacier/glacier_helper.sh
+++ glacier/glacier_helper.sh
@@ -23,9 +23,9 @@ show_menu() {
     echo -e "${BLUE}================= Cytic 管理菜单 =================${NC}"
     echo -e "${PACKAGE_ICON} 1. 安装節點"
     echo -e "${PACKAGE_ICON} 2. 查看日誌"
-    echo -e "${WRENCH_ICON}  3. 刪除節點"
-    echo -e "${WRENCH_ICON}  0. 更新Script"
-    echo -e "🚪 5. 退出"
+    echo -e "${WRENCH_ICON} 3. 刪除節點"
+    echo -e "${WRENCH_ICON} 0. 更新Script"
+    echo -e "🚪 9. 退出"
     echo -e "${BLUE}====================================================${NC}"
     read -p "请选择一个选项 [0-5]: " choice
 }
@@ -71,10 +71,9 @@ update_script(){
 while true; do
     show_menu
     case $choice in
-        1) install_node ;;
+        1) start_node ;;
         2) view_logs ;;
         3) uninstall_node ;;
-        4) update_script ;;
         0) update_script;;
         9) echo -e "${GREEN}退出程序${NC}"; exit 0 ;;
         *) echo -e "${RED}无效选项，请重新输入${NC}";;
