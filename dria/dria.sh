ee72c0f3a11d239ae3af15c8d72aa5ce9953de4e
lionc <shizijian2010@yahoo.com.hk>
2025-01-13 12:54:56 +0800

    add openledger

---
+++ dria/dria.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# 脚本保存路径
+SCRIPT_PATH="$HOME/dria.sh"
+
+# 主菜单函数
+function main_menu() {
+    while true; do
+        clear
+        echo "脚本由大赌社区哈哈哈哈编写，推特 @ferdie_jhovie，免费开源，请勿相信收费"
+        echo "tg频道：https://t.me/Sdohua"
+        echo "================================================================"
+        echo "退出脚本，请按键盘 ctrl + C 退出即可"
+        echo "请选择要执行的操作:"
+        echo "1. 部署dria节点"
+        echo "2. 退出"
+        
+        read -p "请输入选项 (1/2): " choice
+        
+        case $choice in
+            1)
+                run_dkn_compute_launcher
+                ;;
+            2)
+                echo "退出脚本。"
+                exit 0
+                ;;
+            *)
+                echo "无效选项，请重试。"
+                sleep 2
+                ;;
+        esac
+    done
+}
+
+# 运行 dkn-compute-launcher 的函数
+function run_dkn_compute_launcher() {
+    # 检查是否以 root 用户运行脚本
+    if [ "$(id -u)" != "0" ]; then
+        echo "此脚本需要以 root 用户权限运行。"
+        echo "请尝试使用 'sudo -i' 命令切换到 root 用户，然后再次运行此脚本。"
+        exit 1
+    fi
+
+    # 检查 Ollama 是否已安装
+    if command -v ollama &> /dev/null; then
+    echo "Ollama 已安装。"
+    else
+    echo "正在安装 Ollama..."
+    curl -fsSL https://ollama.com/install.sh | sh
+    echo "Ollama 安装完成。"
+    fi
+
+    # 定义文件名
+    FILE="dkn-compute-node"
+
+    # 检查文件是否存在
+    if [ -e "$FILE" ]; then
+        echo "$FILE 存在，正在删除..."
+        rm -rf "$FILE"  # 使用 -rf 以确保删除目录及其内容
+        echo "$FILE 已成功删除。"
+    else
+        echo "$FILE 不存在，无需删除。"
+    fi
+
+    # 下载最新版本的 dkn-compute-launcher
+    echo "正在下载 dkn-compute-node.zip..."
+    curl -L -o dkn-compute-node.zip https://github.com/firstbatchxyz/dkn-compute-launcher/releases/latest/download/dkn-compute-launcher-linux-amd64.zip
+
+    # 解压缩下载的文件
+    echo "正在解压 dkn-compute-node.zip..."
+    unzip dkn-compute-node.zip
+
+    # 进入解压后的目录
+    cd dkn-compute-node || { echo "进入目录失败"; exit 1; }
+
+    # 创建一个新的 screen 会话并运行 ./dkn-compute-launcher
+    echo "正在创建 screen 会话并运行 ./dkn-compute-launcher..."
+    screen -dmS dria ./dkn-compute-launcher
+
+    echo "操作完成，当前目录为: $(pwd)"
+    echo "dkn-compute-launcher 已在 screen 会话 'dria' 中运行。"
+    echo "使用 'screen -r dria' 命令查看运行状态。"
+
+    # 提示用户按任意键返回主菜单
+    read -n 1 -s -r -p "按任意键返回主菜单..."
+}
+
+# 调用主菜单函数
+main_menu
---
+++ openledger/openledger_bot.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# 颜色定义
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # 无颜色
+
+# 图标定义
+CHECK_MARK="✅"
+CROSS_MARK="❌"
+PACKAGE_ICON="📦"
+WRENCH_ICON="🔧"
+KEY_ICON="🔑"
+
+# 变量定义
+NODE_DIR="/root/openledger-bot"
+WALLET_ADDRESS_PATH="/root/initverse/wallet.txt" 
+REPO_ADDRESS="https://github.com/LIONSHI01/openledger-bot.git"
+CONTAINER_NAME="openledger-bot"
+ 
+ 
+# 显示菜单
+show_menu() {
+    echo -e "${BLUE}================= Initverse 管理菜单 =================${NC}"
+    echo -e "${PACKAGE_ICON} 1. 安装機器人"
+    echo -e "${WRENCH_ICON} 2. 查看機器人日志"
+    echo -e "${WRENCH_ICON} 3. 重啟機器人"
+    echo -e "${WRENCH_ICON} 9. 刪除機器人"
+    echo -e "${KEY_ICON} 0. 更新Script"
+    echo -e "🚪 6. 退出"
+    echo -e "${BLUE}====================================================${NC}"
+    read -p "请选择一个选项 [0-9]: " choice
+}
+
+
+
+# 安装 Bot
+install_node() {
+    # provide wallet address
+    read -p "Please input EVM Wallet Address: " WALLET_ADDRESS
+    git clone $REPO_ADDRESS
+    git checkout lion
+    cd $NODE_DIR
+    
+    echo "$WALLET_ADDRESS" > "account.txt"
+
+    docker-compose up -d
+}
+
+restart_node(){
+    cd $NODE_DIR
+    docker-compose restart
+}
+
+ 
+# 查看節點日志
+view_logs() {
+    cd $NODE_DIR
+    docker-compose logs
+}
+ 
+
+ 
+
+
+# 卸载節點
+uninstall_node() {
+    cd $NODE_DIR
+    docker-compose down
+    docker rm $CONTAINER_NAME
+}
+ 
+
+update_script(){
+ wget -O openledger_helper.sh https://raw.githubusercontent.com/LIONSHI01/nodes_management/refs/heads/main/openledger/openledger_helper.sh && chmod +x openledger_helper.sh && ./openledger_helper.sh
+}
+
+
+# 主程序循环
+while true; do
+    show_menu
+    case $choice in
+        1) install_node ;;
+        2) view_logs ;;
+        3) restart_node ;;
+        9) uninstall_node ;;
+        7) echo -e "${GREEN}退出程序${NC}"; exit 0 ;;
+        0) update_script;;
+        *) echo -e "${RED}无效选项，请重新输入${NC}";;
+    esac
+done
